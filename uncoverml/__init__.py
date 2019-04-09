373d7f9e24ea08d2c7556e5824002e715430925a
Sudipta Basak <basaks@gmail.com>
2019-04-10 05:49:19 +1000

    extracted multiscale files

--- uncoverml/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Geoscience Australia Mineral Systems Group, ' \
-             'NICTA Spatial Inference Systems Team'
-__email__ = 'daniel.steinberg@nicta.com.au, basaks@gmail.com'
-__version__ = '0.1.0'
-
